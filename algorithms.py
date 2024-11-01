dc37465f3cccbbc2d5d8e92d9879c80fb76064c7
Janeczenko <janek.wyrzykowski@gmail.com>
2024-11-01 20:01:01 +0100

    finished creating the Dijsktra algorithm

--- algorithms.py
+++ algorithms.py
@@ -12,12 +12,17 @@ def graph_simplify(g: nx.Graph) -> nx.Graph:
     Zwraca:
         `k` (`networkx.Graph`): uproszczony graf pełny.
     """
+
+    # Tworzenie grafu składającego się tylko z wierzchołków wymagających odwiedzenia
     k = nx.Graph()
     k.add_nodes_from([node for (node, node_data) in g.nodes(data=True) if node_data['toVisit']])
 
+    # Dopisanie do grafu `k` krawędzi o optymalnych długościach 
     for node in k.nodes:
         optimal_path_lenghts = dijkstra_optimal_paths(g, node)
-        # TODO dokończyć funkcję
+        k.add_edges_from([(node, key, {'weight': value}) for (key, value) in optimal_path_lenghts.items() if key in k.nodes])
+        
+    return k
 
 def dijkstra_optimal_paths(g: nx.Graph, start: int) -> dict[int, int]:
     """
@@ -31,11 +36,12 @@ def dijkstra_optimal_paths(g: nx.Graph, start: int) -> dict[int, int]:
         `lengths` (`dict[int, int]`): słownik z kluczami będącymi numerami wierzchołków i wartościami będącymi długościami najkrótszych ścieżek między danymi wierzchołkami a wierzchołkiem `start`.
     """
     n = len(g.nodes)
-    # Tworzenie grafu pełnego o krawędziach długości nieskończonej, a następnie napisanie krawędzi tymi z grafu g
+    # Tworzenie grafu pełnego o krawędziach długości nieskończonej, a następnie nadpisanie niektórych krawędzi tymi z grafu g
     h = nx.complete_graph(range(1, n+1))
     h.add_edges_from(h.edges, weight=float('inf'))
     h.add_edges_from(g.edges(data=True))
 
+    # Algorytm Dijkstry
     lengths = {node: h[start][node]['weight'] for node in set(h.nodes) - {start}}
     visited = [start]
 
@@ -49,7 +55,6 @@ def dijkstra_optimal_paths(g: nx.Graph, start: int) -> dict[int, int]:
         for node in set(h.nodes) - set(visited):
             if lengths[node] > mindist + h[minnode][node]['weight']:
                 lengths[node] = mindist + h[minnode][node]['weight']
-    
     return lengths
 
 
