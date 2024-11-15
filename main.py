b128112e22dabc5a81121a65c3d856a8ec723ac3
Janeczenko <janek.wyrzykowski@gmail.com>
2024-11-15 11:51:00 +0100

    various improvements and bug fixes

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import networkx as nx
-import random
-import numpy
 import matplotlib.pyplot as plt
-# import algorithms as alg
+import algorithms as alg
 import graph_generation as gen
 
 def main():
     G = gen.graph_generation()
-
-    print(G)
     nx.draw(G)
-
     plt.show()
-    print(G.nodes(data=True))
+    
+    H = alg.graph_simplify(G)
+    nx.draw(H)
+    plt.show()
 
+    print(H.number_of_nodes())
+    # print(G.edges(data=True))
+    # print(H.edges(data=True))
+
+    path_opt = alg.solve_traveling_salesman(H)
+    print(path_opt)
 
 if __name__ == "__main__":
     main()
